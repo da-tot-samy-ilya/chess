3112332873ab47fbfa3c1a84466a1fb67d893ba9
Alexandra <103366623+fukurome@users.noreply.github.com>
2022-04-17 16:02:37 +0500

    fix problems with versions

--- figures-logic/Chess/Rook.h
+++ figures-logic/Chess/Rook.h
@@ -4,20 +4,11 @@
 
 class Rook : public Piece
 {
-<<<<<<< HEAD
-public: 
-    Rook(bool col, int vert, int hor) : Piece(col, vert, hor) {};
-||||||| a5ec6b7
-public:
-    Rook(bool col, int vert, int hor) : Piece(col, vert, hor) {};
-=======
 public:
     Rook(char col, int vert, int hor) : Piece(col, vert, hor) {};
->>>>>>> fd5c32c0c228a44f6c22ad2cbed549f412af9969
     Rook(Rook& f) : Piece(f.colour, f.vertical, f.horizontal) {};
     int move(int vert, int hor) override;
     int cut_down(Piece& f) override;
-    bool getFirstMove() { return didMove };
 };
 
 int Rook::move(int vert, int hor)
